d130a66404f4d3c6306440b3900ea58a1310b888
Andrii Yanakov <yanak1984@gmail.com>
2025-07-18 13:32:34 +0300

    Restore the original code formatting

--- commands/root.go
+++ commands/root.go
@@ -39,81 +39,30 @@ func init() {
 
 	fs := rootCmd.Flags()
 	fs.SortFlags = false
-	fs.StringVarP(&extractCfg.ExtractFormat,
-		"format",
-		"f",
-		def.ExtractFormat,
-		"Output format of the extraction. Valid values are 'pot' and 'json'.",
-	)
+	fs.StringVarP(&extractCfg.ExtractFormat, "format", "f", def.ExtractFormat, "Output format of the extraction. Valid values are 'pot' and 'json'.")
 	fs.StringVarP(&extractCfg.SourceDir, "directory", "D", def.SourceDir, "Directory with the Go source files")
-	fs.StringVarP(&extractCfg.OutputDir,
-		"output-dir",
-		"p",
-		def.OutputDir,
-		"Directory in which the pot files are stored.",
-	)
+	fs.StringVarP(&extractCfg.OutputDir, "output-dir", "p", def.OutputDir, "Directory in which the pot files are stored.")
 	fs.StringVarP(&extractCfg.OutputFile, "output", "o", def.OutputFile, "Write output to specified file")
-	fs.StringSliceVarP(&extractCfg.CommentPrefixes,
-		"add-comments",
-		"c",
-		def.CommentPrefixes,
-		"Place comment blocks starting with TAG and preceding keyword lines in output file",
-	)
-	fs.BoolVarP(&extractCfg.ExtractErrors,
-		"extract-errors",
-		"e",
-		def.ExtractErrors,
-		"Strings from errors.New(STRING) are extracted",
-	)
-	fs.StringVar(&extractCfg.ErrorContext,
-		"errors-context",
-		def.ErrorContext,
-		"Context which is automatically assigned to extracted errors",
-	)
+	fs.StringSliceVarP(&extractCfg.CommentPrefixes, "add-comments", "c", def.CommentPrefixes, "Place comment blocks starting with TAG and preceding keyword lines in output file")
+	fs.BoolVarP(&extractCfg.ExtractErrors, "extract-errors", "e", def.ExtractErrors, "Strings from errors.New(STRING) are extracted")
+	fs.StringVar(&extractCfg.ErrorContext, "errors-context", def.ErrorContext, "Context which is automatically assigned to extracted errors")
 
-	fs.StringArrayVarP(&extractCfg.TemplatePatterns,
-		"template-directory",
-		"t",
-		[]string{},
-		"Set a list of paths to which the template files contain. Regular expressions can be used.",
-	)
+	fs.StringArrayVarP(&extractCfg.TemplatePatterns, "template-directory", "t", []string{}, "Set a list of paths to which the template files contain. Regular expressions can be used.")
 	fs.String("template-prefix", "", "Sets a prefix for the translation functions, which is used within the templates")
-	fs.BoolVar(&extractCfg.TmplIsMonolingual,
-		"template-use-kv",
-		false,
-		"Determines whether the strings from templates should be handled as key-value",
-	)
-	fs.StringArrayP("template-keyword",
-		"k",
-		[]string{},
-		"Sets a keyword that is used within templates to identify translation functions",
-	)
+	fs.BoolVar(&extractCfg.TmplIsMonolingual, "template-use-kv", false, "Determines whether the strings from templates should be handled as key-value")
+	fs.StringArrayP("template-keyword", "k", []string{}, "Sets a keyword that is used within templates to identify translation functions")
 
-	fs.StringVarP(&extractCfg.DefaultDomain,
-		"default-domain",
-		"d",
-		def.DefaultDomain,
-		"Use name.pot for output (instead of messages.pot)",
-	)
+	fs.StringVarP(&extractCfg.DefaultDomain, "default-domain", "d", def.DefaultDomain, "Use name.pot for output (instead of messages.pot)")
 	fs.BoolVar(&extractCfg.WriteNoLocation, "no-location", def.WriteNoLocation, "Do not write '#: filename:line' lines")
 
 	fs.IntVarP(&extractCfg.WrapWidth, "width", "w", def.WrapWidth, "Set output page width")
-	fs.BoolVar(&extractCfg.DontWrap,
-		"no-wrap",
-		def.DontWrap,
-		"Do not break long message lines, longer than the output page width, into several lines",
-	)
+	fs.BoolVar(&extractCfg.DontWrap, "no-wrap", def.DontWrap, "Do not break long message lines, longer than the output page width, into several lines")
 
 	fs.BoolVar(&extractCfg.OmitHeader, "omit-header", def.OmitHeader, "Don't write header with 'msgid \"\"' entry")
 	fs.StringVar(&extractCfg.CopyrightHolder, "copyright-holder", def.CopyrightHolder, "Set copyright holder in output")
 	fs.StringVar(&extractCfg.PackageName, "package-name", def.PackageName, "Set package name in output")
 	fs.StringVar(&extractCfg.BugsAddress, "msgid-bugs-address", def.BugsAddress, "Set report address for msgid bugs")
-	fs.StringSliceVarP(&extractCfg.LoadedPackages,
-		"loaded-packages",
-		"l",
-		[]string{},
-		"List of packages divided by comma to search for translations",
-	)
+	fs.StringSliceVarP(&extractCfg.LoadedPackages, "loaded-packages", "l", []string{}, "List of packages divided by comma to search for translations")
 }
 
 func initVersionNumber() {
